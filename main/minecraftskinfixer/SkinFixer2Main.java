9259b6ba505004fd1a83b3e5e6c351dd92b5e990
sunung0110 <79097389+sunung0110@users.noreply.github.com>
2021-07-06 09:51:31 +0900

    Update SkinFixer2Main.java

--- main/minecraftskinfixer/SkinFixer2Main.java
+++ main/minecraftskinfixer/SkinFixer2Main.java
@@ -2,7 +2,7 @@ package minecraftskinfixer;
 
 public class SkinFixer2Main {
 	
-	public static final String VERSION = "2.4";
+	public static final String VERSION = "2.5";
 	
 	private static SuperSimpleLogger logger;
 	
